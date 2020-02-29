031b56cc490f9653647e2f82cfd832adc2178e8e
Ming <itsok0@naver.com>
2020-03-01 01:59:35 +0900

    20.03.01:01:59

--- app/src/main/java/com/example/nslngiot/widget/widget_provider.java
+++ app/src/main/java/com/example/nslngiot/widget/widget_provider.java
@@ -7,6 +7,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
@@ -29,41 +31,21 @@ import java.util.Map;
 public class widget_provider extends AppWidgetProvider {
 
     private static final String StatusReflash = "com.example.nslngiot.imgbtn_widget_reflash";
-
+    private Intent reflash = new Intent(StatusReflash);
     public boolean person;
     public boolean water;
     public boolean coffe;
     public boolean a4;
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        // Construct the RemoteViews object
-
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-        Intent reflash = new Intent(StatusReflash);
-        PendingIntent pendingIntent_reflash = PendingIntent.getBroadcast(context, 0, reflash, PendingIntent.FLAG_CANCEL_CURRENT);
-        views.setOnClickPendingIntent(R.id.imgbtn_widget_reflash, pendingIntent_reflash);
-
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-        ComponentName thisAppWidget = new ComponentName(context.getPackageName(), widget_provider.class.getName());
-        int[] appWidgets = appWidgetManager.getAppWidgetIds(thisAppWidget);
-        //imgview_person = views.findViewById(R.id.img_person);
         final String action = intent.getAction();
+
         if (action.equals(StatusReflash)) {//새로고침 버튼 클릭시
-
-            check(context);
-
+            Status_SelectRequest(context);//volley 호출
 
             if (person == true) {
                 views.setImageViewResource(R.id.img_person, R.drawable.people_exist);
@@ -82,37 +64,34 @@ public class widget_provider extends AppWidgetProvider {
             } else if (coffe == false) {
                 views.setImageViewResource(R.id.img_coffee, R.drawable.coffee_nonexist);
             }
-
             if (a4 == true) {
                 views.setImageViewResource(R.id.img_a4, R.drawable.a4_exist);
             } else if (a4 == false) {
                 views.setImageViewResource(R.id.img_a4, R.drawable.a4_nonexist);
             }
 
-            AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, widget_provider.class), views);
+           AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, widget_provider.class), views);//위젯 업데이트
         }
 
-        /* else if (action.equals(ACTION_BUTTON3)){
-            Toast.makeText(context, "이미지를 교체 할께요.", Toast.LENGTH_SHORT).show();
-            //AsyncTask를 이용해서 이미지를 가져와서 교체해 보자.
-            String imgUrl = "http:///교체할이미지주소.png";
-            new DownloadBitmap(views, appWidgets[0], appWidgetManager).execute(imgUrl); //AsyncTask 실행
-            AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, NewAppWidget.class), views);
-        }*/
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);//리모트뷰 지정?
+        //위젯 버튼 등록?
+        PendingIntent pendingIntent_reflash = PendingIntent.getBroadcast(context, 0, reflash, PendingIntent.FLAG_CANCEL_CURRENT);
+        views.setOnClickPendingIntent(R.id.imgbtn_widget_reflash, pendingIntent_reflash);
+
+        appWidgetManager.updateAppWidget(appWidgetIds, views);//위젯 업데이트
+
 
     }
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+        // Enter relevant functionality for when the first widget is create
     }
 
     @Override
@@ -120,13 +99,7 @@ public class widget_provider extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    public void check(Context context) {
-        Status_SelectRequest(context);
-
-    }
-
-    public  synchronized void Status_SelectRequest(final Context context) {
-        // views = new RemoteViews(context.getPackageName(), R.layout.widget);
+    public void Status_SelectRequest(final Context context) {
 
         //다시 확인
         StringBuffer url = new StringBuffer("http://210.125.212.191:8888/IoT/DoorStatusCheck.jsp");
@@ -134,8 +107,11 @@ public class widget_provider extends AppWidgetProvider {
 
         StringRequest stringRequest = new StringRequest(
 
+
                 Request.Method.POST, String.valueOf(url),
                 new Response.Listener<String>() {
+
+
                     boolean person = false;
                     boolean water = false;
                     boolean a4 = false;
@@ -148,18 +124,19 @@ public class widget_provider extends AppWidgetProvider {
                         Log.d("진입", response);
                         switch (response) {
                             case "open":
-                                Toast.makeText(context, "ㅇㅇㅇㅇㅇ", Toast.LENGTH_LONG).show();
+                                // Toast.makeText(context, "ㅇㅇㅇㅇㅇ", Toast.LENGTH_SHORT).show();
                                 person = true;
                                 break;
                             case "close":
-                                Toast.makeText(context, "ㄴㄴㄴㄴ", Toast.LENGTH_LONG).show();
+                                // Toast.makeText(context, "ㄴㄴㄴㄴ", Toast.LENGTH_SHORT).show();
                                 person = false;
                                 break;
                             case "error":
-                                Toast.makeText(context, "다시 시도해주세요.", Toast.LENGTH_LONG).show();
+                                Toast.makeText(context, "다시 시도해주세요.", Toast.LENGTH_SHORT).show();
                                 break;
                         }
-                        setStatus(person, water, coffe, a4);
+
+                        setStatus(person, water, coffe, a4, context);
                     }
 
                 },
@@ -172,6 +149,7 @@ public class widget_provider extends AppWidgetProvider {
         ) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
+                System.out.println("2");
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("check", "security");
 
@@ -185,13 +163,16 @@ public class widget_provider extends AppWidgetProvider {
         VolleyQueueSingleTon.getInstance(context).addToRequestQueue(stringRequest);
     }
 
-    public void setStatus(boolean person, boolean water, boolean coffe, boolean a4) {
-        this.person =person;
-        this.water = water;
-        this.coffe = coffe;
-        this.a4 =a4;
+    public void setStatus(boolean person, boolean water, boolean coffe, boolean a4, Context context) {
+
+        if (person != this.person || water != this.water || coffe != this.coffe || a4 != this.a4) {//하나라도 상태가 다를 시 실행.
+            this.person = person;
+            this.water = water;
+            this.coffe = coffe;
+            this.a4 = a4;
+            this.onReceive(context, reflash);//Receive를 다시 호출하여 위젯 이미지를 바꾸게 함.
+        }
+
 
     }
 }
-
-
